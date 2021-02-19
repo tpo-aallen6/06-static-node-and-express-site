bb069f8f32020d9f5e17b1e81cf4124424104897
Adam Allen <aallen6@travelers.com>
2021-02-19 12:32:44 -0500

    fix pictures and add error handlers

--- app.js
+++ app.js
@@ -21,6 +21,36 @@ app.get('/project/:id', (req, res) => {
   res.render('project', projectData.projects[projectIndex])
 })
 
+app.use((req, res, next) => {
+  const err = new Error('That page does not exist, please go back.')
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+
+  if (err.status === 404) {
+    console.log('Sorry, that page was not found.')
+    res.render('page-not-found', {
+      err: {
+        message: err.message,
+        status: err.status
+      }
+    })
+  } else {
+    err.message = 'Sorry, something went wrong. PLease try again'
+    err.status = 500
+    console.log('Sorry, something went wrong. Please try again.')
+    res.render('error', {
+      err: {
+        message: err.message,
+        status: err.status
+      }
+    })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}...`)
 })
