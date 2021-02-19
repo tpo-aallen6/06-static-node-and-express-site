bae3484d65077c7dde8f228ca2c34881c3be6684
Adam Allen <aallen6@travelers.com>
2021-02-18 21:01:09 -0500

    add data
    added data to data.json.
    added text content to pug templates.
    added project 6 to data.json
    change some css styling

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,21 @@
 const body = document.querySelector('body')
 let headerBtnClicked = false
 
+document.querySelectorAll('.portfolio-intro')
+  .forEach(element => {
+    element.style.backgroundColor = '#abdae6'
+  })
+
+document.querySelectorAll('.portfolio-intro p')
+  .forEach(element => {
+    element.style.color = 'black'
+  })
+
+document.querySelectorAll('.cell')
+  .forEach(element => {
+    element.style.boxShadow = '5px 5px #888888'
+  })
+
 document.querySelector('#menu-icon').addEventListener('click', e => {
   !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)'
   return headerBtnClicked = !headerBtnClicked
